fab8165e5dd0baf12f69f6483794d2c4adcf3ed8
Nyika Benedek <benikecske@gmail.hu>
2021-04-09 15:53:56 +0200

    main.java update: basic logging and markers added

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,24 @@
-public class Main {
-    public static void main(String[] args) {
+import org.apache.logging.log4j.*;
 
+public class Main {
+    private static final Logger logger = LogManager.getLogger();
+    private static final Marker KIJELENT = MarkerManager.getMarker("KIJELENT");
+    private static final Marker KERELEM = MarkerManager.getMarker("KÉRELEM");
+
+    public static void main(String[] args) {
+        int iterationCount = 2;
+        if(args.length == 1){
+            iterationCount = Integer.parseInt(args[0]);
+        }
+        for (int i = 1; i <= iterationCount; i++){
+            ThreadContext.put("iterationNumber", String.valueOf(i));
+            logger.trace(KIJELENT, "Jó reggelt, jó reggelt,");
+            logger.debug(KIJELENT, "Kedves liliomszál,");
+            logger.info(KIJELENT,"Megöntözlek rózsavízzel,");
+            logger.warn("Hogy ne hervadozzál.");
+            logger.error(KERELEM, "Szabad-e locsolni?");
+
+            ThreadContext.clearAll();
+        }
     }
 }
